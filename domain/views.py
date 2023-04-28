c2c560ddec18cdbfc8519f942e912d55fb7f8041
sudharaathith <t.sudharaathith@gmail.com>
2023-04-28 09:07:39 +0530

    init frontend

--- domain/views.py
+++ domain/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from Scraper.Creater import save_domine
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['POST'])
+def Save_domine(request):
+    data = request.data
+    if save_domine(data):
+        return Response(data, status=200)
+    else:
+        return Response(data, status=500)
+    
