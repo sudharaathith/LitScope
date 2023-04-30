5c57a40b296cad166f5ca37d69d560d25152c5c1
sudharaathith <t.sudharaathith@gmail.com>
2023-05-01 00:24:26 +0530

    ui

--- frontend/src/Components/SearchApi.jsx
+++ frontend/src/Components/SearchApi.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import InputBox from './InputBox';
+import { Input } from "@material-tailwind/react";
+import { useState } from 'react';
+import validator from 'validator'
 
 function SearchApi(props) {
+    let [url , setUrl] = useState("");
     return (
+
         <div>
             <div className='flex flex-row  items-center'>
             <div className="ml-10 w-1/2  items-center">
                 Domine name
             </div>
-            <div className=" absolute left-60">
-            <InputBox className="" text="domain name" /></div>
+            <div className=" absolute left-60 w-96" >
+            <Input 
+                success={(validator.matches(url,"^https?://(?:[a-z0-9-]+\\.)+[a-z]{2,}(?:/[^/\\s]*)+((//<<Query>>//)?)$"))}
+                error={!(validator.matches(url,"^https?://(?:[a-z0-9-]+\\.)+[a-z]{2,}(?:/[^/\\s]*)+((//<<Query>>//)?)$"))}
+                label='Url'
+                onChange={(e) => {
+                    setUrl(e.target.value)
+                }}
+            />
+        </div>
         </div>
         </div>
     );
