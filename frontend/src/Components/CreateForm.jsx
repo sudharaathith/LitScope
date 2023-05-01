486328ab1d2d6790ab33f7ec427a3559c3336732
sudharaathith <t.sudharaathith@gmail.com>
2023-05-01 12:27:07 +0530

    ui and djngo connect

--- frontend/src/Components/CreateForm.jsx
+++ frontend/src/Components/CreateForm.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import InputBox from './InputBox';
-import { Select, Option } from "@material-tailwind/react";
+import { Select, Option, Button } from "@material-tailwind/react";
 import SearchApi from './SearchApi';
 
 function CreateForm(props) {
+    let [domainName, setDomainName] = useState("");
     let [mode, setMode] = useState("");
+    let [search, setSearch] = useState({});
+
     return (
         <div className='flex flex-col justify-center mt-3'>
         <div className='flex flex-row  items-center'>
@@ -12,7 +15,9 @@ function CreateForm(props) {
                 Domine name
             </div>
             <div className=" absolute left-60">
-            <InputBox className="" text="domain name" /></div>
+            <InputBox className="" onChange={(val)=>{
+                setDomainName(val);
+            }} text="domain name" /></div>
         </div>
         <div className='flex flex-row  mt-3'>
             <div className="ml-10 items-center">
@@ -28,7 +33,32 @@ function CreateForm(props) {
         </div>
         </div>
         <div className=' mt-9'>
-       {(mode=='api')?<SearchApi/>:null}
+       {(mode==='api')?<SearchApi onChange={(val)=>{
+            setSearch(val);
+       }}/>:null}
+        </div>
+        <div className='ml-8 mt-10'>
+        {!(mode==='')?<Button onClick={()=>{
+            let data = {
+            "domine_name":domainName,
+            "mode":mode,
+            "search":search
+        }
+
+        fetch("http://127.0.0.1:8000/api/domain/create/",{
+            method: "POST",
+            headers: {
+    'Content-Type': 'application/json'
+    // Other possible headers
+  },
+            body: JSON.stringify(data),
+            mode:'no-cors'
+            }
+
+        )
+        }}>
+            Save
+        </Button>:null}
         </div>
         </div>
     );
--- frontend/src/Components/SearchApi.jsx
+++ frontend/src/Components/SearchApi.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { Input } from "@material-tailwind/react";
+import React, { useEffect } from 'react';
+import { Input, Select, Option } from "@material-tailwind/react";
 import { useState } from 'react';
 import validator from 'validator'
 import Route from './Route';
 
 function SearchApi(props) {
     let [url , setUrl] = useState("");
+    let [methode, setMethode] = useState("");
+    let [route, setRoute] = useState([]);
+
+    
+    let update = ()=>{
+        props.onChange({
+            "url":url,
+            "route":route,
+            "methode":methode
+        });
+    }
+    useEffect(update,[url,methode,route]);
     return (
 
         <div>
             <div className='flex flex-row  items-center'>
             <div className="ml-10 w-1/2  items-center">
-                Domine name
+                Url
             </div>
             <div className=" absolute left-60 w-96" >
             <Input 
@@ -19,18 +31,33 @@ function SearchApi(props) {
                 error={!(validator.matches(url,"^https?://(?:[a-z0-9-]+\\.)+[a-z]{2,}(?:/[^/\\s]*)+((//<<Query>>//)?)$"))}
                 label='Url'
                 onChange={(e) => {
-                    setUrl(e.target.value)
+                    setUrl(e.target.value);
                 }}
             />
         </div>
         </div>
         <div className='flex flex-row mt-6  items-center'>
             <div className="ml-10 w-1/2  items-center">
-                Domine name
+                Route
             </div>
-            <div className=" absolute left-60 w-96" >
-           <Route/>
             
+        </div>
+            <div className=" absolute mt-5 left-10 " >
+           <Route onChange={(val) =>{
+            setRoute(val);
+           }}/>
+        </div>
+        <div className='flex flex-row mt-24'>
+            <div className="ml-10 items-center">
+                mode
+            </div>
+            <div className=" absolute left-60">
+        <Select label='mode' onChange={(e)=>{
+            setMethode(e)
+        }}>
+            <Option value='GET'>GET</Option>
+            <Option value='POST'>POST</Option>
+        </Select>
         </div>
         </div>
         </div>
