2550dee44409fcb67d15d36aac44da761d8eb5dd
sudharaathith <t.sudharaathith@gmail.com>
2023-05-30 21:30:33 +0530

    ui

--- frontend/src/Components/Searchresult.jsx
+++ frontend/src/Components/Searchresult.jsx
@@ -3,7 +3,7 @@ import { Typography, Chip } from "@material-tailwind/react";
 function Searchresult() {
   let colors = ["blue","red", "green",  "amber", "pink", "indigo", "purple", "teal", "cyan"]
   return (
-    <div className='   p-5 rounded-2xl shadow-xl m-5 flex flex-col '>
+    <div className='   p-5 rounded-2xl shadow-xl m-5 flex flex-col h-[75vh] '>
       <div>
         <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
         <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
@@ -13,7 +13,7 @@ function Searchresult() {
         <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
 
         <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} /></div>
-        <div className='  shadow-inner rounded-xl flex flex-col overflow-hidden my-3'>
+        <div className='  shadow-inner rounded-xl flex flex-col overflow-y-scroll my-3'>
           <Option />
           <Option />
           <Option />
