a3d05f346f3270285acfbdaef47e9d64a8444f18
sudharaathith <t.sudharaathith@gmail.com>
2023-05-30 21:26:09 +0530

    ui

--- frontend/src/Components/Searchresult.jsx
+++ frontend/src/Components/Searchresult.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
-import { Typography } from "@material-tailwind/react";
+import { Typography, Chip } from "@material-tailwind/react";
 function Searchresult() {
+  let colors = ["blue","red", "green",  "amber", "pink", "indigo", "purple", "teal", "cyan"]
   return (
-    <div className='m-10'>
-     <Typography variant="h3">SEARCH</Typography>
+    <div className='   p-5 rounded-2xl shadow-xl m-5 flex flex-col '>
+      <div>
+        <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
+        <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
+        <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
+        <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
+        <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
+        <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} />
+
+        <Chip className=' m-1' color={colors[Math.floor(Math.random() * 9)] } value={"summa"} /></div>
+        <div className='  shadow-inner rounded-xl flex flex-col overflow-hidden my-3'>
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+        </div>
+
      </div>
+
   )
 }
+
+
+ function Option() {
+  return (
+    <div className=' w-full h-fit text-center p-3 shadow-sm'>Searchresult</div>
+  )
+}
+
 
 export default Searchresult
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,21 +1,27 @@
-import React, { Component } from 'react'
-import NavBar from '../Components/NavBar';
-import Searchbar from '../Components/Searchbar';
-import Searchresult from '../Components/Searchresult';
+import React, { Component } from "react";
+import NavBar from "../Components/NavBar";
+import Searchbar from "../Components/Searchbar";
+import Searchresult from "../Components/Searchresult";
 
 function Home(props) {
-    return (
-        <div>
-         <NavBar />  
-         <br/>
-         <br/>
-         <br/>
-         <br/>
-         <div><Searchbar/></div>
-         <br/>
-         <div><Searchresult/></div>
+  return (
+    <div className="h-screen overflow-hidden">
+      <NavBar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div>
+        <Searchbar />
+      </div>
+      <br />
+      <div className="flex flex-row h-full">
+        <div className=" w-1/2">
+        <Searchresult />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
