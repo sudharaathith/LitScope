4c6080e95fdf1fb9583979df50350dd14a0c4d55
vigneshwaran2605 <vigneshnethaji26@gmail.com>
2023-05-31 01:07:00 +0530

    searchupdate

---
+++ keyword_search.py
@@ -0,0 +1,34 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import pandas as pd
+
+def extract_keywords(text):
+    # Tokenize the text into individual words
+    tokens = word_tokenize(text)
+    
+    # Remove stop words (common words that don't carry much meaning)
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+    
+    # Perform part-of-speech tagging to identify nouns and adjectives
+    tagged_tokens = nltk.pos_tag(filtered_tokens)
+    
+    # Extract keywords based on the part-of-speech tags
+    keywords = [word for word, tag in tagged_tokens if tag.startswith('NN') or tag.startswith('JJ')]
+    tech_key=[]
+    a = pd.read_csv("tech.csv")
+    for i in keywords:
+        b = a[a["name"].str.lower().str.match(i)]["name"].to_list()
+        if b!= []:
+            tech_key.append(b[0])  
+    return tech_key
+
+
+# Example usage
+user_input = input("Enter a sentence: ")
+print(extract_keywords(user_input))
+
+# a = pd.read_csv("tech.csv")
+# for i in keywords:
+#     print(a[a["name"].str.lower().str.match(i)]["name"].to_list())    
